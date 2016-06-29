9d70c6cf22fdceb51991c6e59c9e8fb8ee3e4a4f
VadimKey <polecatov@gmail.com>
2016-06-30 01:12:28 +0300

    yet another stub, for input from T/N

---
+++ stub-tn.cxx
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef uint64_t Int;
+
+Int solve(Int N) {
+  return 0;
+}
+
+int main() {
+  int T;
+  cin >> T;
+  
+  for (int i = 0; i < T; ++i) {
+    Int N;
+    cin >> N;
+    cout << solve(N) << endl;
+  }
+  
+  return 0;
+}
